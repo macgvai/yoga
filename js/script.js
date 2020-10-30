b1e7c119df95824f05a5ecbda2d2cef39b55a500
Victor <st.viktorr@gmail.com>
2020-10-30 12:15:49 +0300

    add days in timer finish

--- js/script.js
+++ js/script.js
@@ -45,8 +45,8 @@ window.addEventListener('DOMContentLoaded', function() {
         let t = Date.parse(endtime) - Date.parse(new Date()),
             seconds = Math.floor((t/1000) % 60),
             minutes = Math.floor((t/1000/60) % 60),
-            hours = Math.floor((t/(1000*60*60)) % 24),
-            days = Math.floor((t/1000/60/60/24));
+            hours = Math.floor((t/(1000 * 60 * 60)) % 24),
+            days = Math.floor((t/(1000 * 60 * 60 * 24)));
 
             return {
                 'total' : t,
@@ -56,7 +56,8 @@ window.addEventListener('DOMContentLoaded', function() {
                 'days' : days
             };
     }
-    
+    console.log(new Date());
+    console.log(deadLine);
 
 
     
