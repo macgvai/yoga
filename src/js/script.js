68780ca692ebfd701d44bcbd6377a7e05d828b09
Victor <st.viktorr@gmail.com>
2020-11-20 13:41:24 +0300

    webpack

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,8 @@
+require('es6-promise/auto');
+require('nodelist-foreach-polyfill');
+require('formdata-polyfill');
+
+
 window.addEventListener('DOMContentLoaded', function() {
 
     'use strict';
@@ -7,8 +12,8 @@ window.addEventListener('DOMContentLoaded', function() {
         slider = require('./parts/slider'),
         tabs = require('./parts/tabs'),
         timer = require('./parts/timer');
-
-
+ 
+ 
     calc();
     form();
     modal();
